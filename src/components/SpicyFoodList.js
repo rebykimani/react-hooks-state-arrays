9373317ffaa77905eb6e3a6ec44db7184fdbb32c
rebykimani <beckireby15@gmail.com>
2022-10-05 22:43:24 +0300

    completed assignment

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -1,16 +1,22 @@
 import React, { useState } from "react";
-import { spicyFoods, getNewRandomSpicyFood } from "../data";
+import { spicyFoods, getNewRandomSpicyFood} from "../data";
 
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
+    const newFoodArray = [...foods, newFood];
+    setFoods(newFoodArray);
   }
 
+  function handleClick(id) {
+   const newFoodArray = foods.filter((food) => food.id !== id);
+   setFoods(newFoodArray);
+ }
+
   const foodList = foods.map((food) => (
-    <li key={food.id}>
+    <li key={food.id} onClick={() => handleClick(food.id)} >
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
